b91f53b5c5d7b92a19cd5ef5b602ea681c110d95
wen <1759974971@qq.com>
2021-04-20 20:54:31 +0800

    servlet坑

--- src/work/servlet/sing/ServletLogIn.java
+++ src/work/servlet/sing/ServletLogIn.java
@@ -5,7 +5,6 @@ import work.tool.sing.property.UserProperty;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
-import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.util.Base64;
 
